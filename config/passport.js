907740206fcc0662af19137a7aaeeec61a3ae16a
Howhowchen <debby8698@gmail.com>
2022-09-19 02:27:54 +0800

    feat: add facebook login & dotenv

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const passport = require('passport')
 const User = require('../models/user')
 const LocalStrategy = require('passport-local').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy
 const bcrypt = require('bcryptjs')
 module.exports = app => {
   //  初始化 Password.js模組
@@ -11,17 +12,42 @@ module.exports = app => {
     User.findOne({ email })
       .then(user => {
         if (!user) {
-          return done(null, false, { message: 'This email is not registered' })
+          return done(null, false, req.flash('warning_msg', 'Incorrect email or password.'))
         }
         return bcrypt.compare(password, user.password).then(isMatch => {
           if (!isMatch) {
-            return done(null, false, { message: 'Email or Password incorrect.' })
+            return done(null, false, req.flash('warning_msg', 'Incorrect email or password.'))
           }
           return done(null, user)
         })
       })
       .catch(err => done(err, false))
   }))
+
+  passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: process.env.FACEBOOK_CALLBACK,
+    profileFields: ['email', 'displayName']
+  }, (accessToken, refreshToken, profile, done) => {
+    const { name, email } = profile._json
+    User.findOne({ email })
+      .then(user => {
+        if (user) return done(null, user)
+        const randomPassword = Math.random().toString(36).slice(-8)
+        bcrypt
+          .genSalt(10)
+          .then(salt => bcrypt.hash(randomPassword, salt))
+          .then(hash => User.create({
+            name,
+            email,
+            password: hash
+          }))
+          .then(user => done(null, user))
+          .catch(err => done(err, false))
+      })
+  }))
+
   //  設定序列化與反序列化
   passport.serializeUser((user, done) => {
     done(null, user.id)
